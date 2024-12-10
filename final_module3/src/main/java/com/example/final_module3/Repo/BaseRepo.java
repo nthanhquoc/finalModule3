99e24324735e1a7a284284d3ec0b185681b9c6ac
nthanhquoc60 <nthanhquoc60@gmail.com>
2024-12-10 21:08:29 +0700

    finalmodule

---
+++ final_module3/src/main/java/com/example/final_module3/Repo/BaseRepo.java
@@ -0,0 +1,29 @@
+package com.example.final_module3.Repo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class BaseRepo  {
+    private static String jdbcURL = "jdbc:mysql://localhost:3306/product?useSSL=false";
+    private static String jdbcUsername = "root";
+    private static String jdbcPassword = "123456";
+    private static Connection connection = null;
+    static {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private BaseRepo() {
+    }
+
+    public static Connection getConnection() {
+        return connection;
+    }
+}
