cb3df5d0c32dc1978b5f2d8db9e340800b578968
nthanhquoc60 <nthanhquoc60@gmail.com>
2024-12-11 11:11:06 +0700

    Sua bai

--- final_module3/src/main/java/com/example/final_module3/Service/implement/BorrowService.java
+++ final_module3/src/main/java/com/example/final_module3/Service/implement/BorrowService.java
@@ -10,13 +10,6 @@ import java.util.List;
 public class BorrowService implements IBorrowService {
     private static List<Student> students = new ArrayList<>();
 
-    static {
-        // Dummy data
-        students.add(new Student(1, "Alice Johnson", "Class A"));
-        students.add(new Student(2, "Bob Smith", "Class B"));
-        students.add(new Student(3, "Charlie Brown", "Class A"));
-    }
-
     @Override
     public List<Student> getAllStudents() {
         return students;
@@ -24,6 +17,5 @@ public class BorrowService implements IBorrowService {
 
     @Override
     public void addBorrow(Borrow borrow) {
-        // Logic to save borrow details to the database
     }
 }
